5c7990020a39f60a4e75a7a3ca3ae0897ef1a982
Christopher Rasch-Olsen Raa <christopher@stix.no>
2023-02-05 09:26:43 +0100

    Some whitespace cleanup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,13 +14,13 @@
 ;; I don't really use it though.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
-  (load custom-file))
+	(load custom-file))
 
 ;; Read any private settings from my private.el file. This will not be in git.
-(setq private-file (expand-file-name "private.el" user-emacs-directory))
+(defvar private-file (expand-file-name "private.el" user-emacs-directory)
+	"Full path to the private.el file that private variables are loaded from.")
 (when (file-exists-p private-file)
 	(load private-file))
-
 
 ;; Define and initialise package repositories. Primarily using Melpa, but more will
 ;; be added when needed.
@@ -30,8 +30,8 @@
 
 ;; Use use-package to simplify the installation and initialization of packages I use
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+	(package-refresh-contents)
+	(package-install 'use-package))
 (require 'use-package)
 ;; Makes it so I don't have to add an `:ensure` tag to all use-package blocks
 (setq use-package-always-ensure 't)
@@ -44,109 +44,106 @@
 
 ;; Enable the exotica theme, as that is quite pleasant to use
 (use-package exotica-theme
-  :config (load-theme 'exotica t))
+	:config (load-theme 'exotica t))
 
 ;; Org-mode is basically an essential part of my kit and day-to-day work. The config is
 ;; somewhat large, but the productivity gains are amazing.
 (use-package org
-  :commands (org-capture org-agenda)
+	:commands (org-capture org-agenda)
 	:custom
-  (org-agenda-start-with-log-mode t)
-  ;; Configure custom agenda views
-  (org-agenda-custom-commands
+	(org-agenda-start-with-log-mode t)
+	;; Configure custom agenda views
+	(org-agenda-custom-commands
 	 ;; Dashboard view is quite nice, giving me my agenda for the day, active tasks I am
 	 ;; working on plus a list of the tasks I have marked as my next tasks
-   '(("d" "Dashboard"
+	 '(("d" "Dashboard"
 			((agenda "" ((org-deadline-warning-days 7)))
-       (todo "ACTIVE"
-						 ((org-agenda-overriding-header "Active Tasks")))
-       (todo "NEXT"
-						 ((org-agenda-overriding-header "Next Tasks")))
-       ))
+			 (todo "ACTIVE" ((org-agenda-overriding-header "Active Tasks")))
+			 (todo "NEXT" ((org-agenda-overriding-header "Next Tasks")))
+			 ))
 
 		 ;; Simply shows a list of any active tasks.
-     ("n" "Active Tasks"
-			((todo "ACTIVE"
-						 ((org-agenda-overriding-header "Next Tasks")))))
+		 ("n" "Active Tasks"
+			((todo "ACTIVE" ((org-agenda-overriding-header "Next Tasks")))))
 
 		 ;; Similarly shows me a list of all my next tasks
-     ("n" "Next Tasks"
+		 ("n" "Next Tasks"
 			((todo "NEXT"
-						 ((org-agenda-overriding-header "Next Tasks")))))
+				((org-agenda-overriding-header "Next Tasks")))))
 
 		 ;; All tasks tagged as being related to work. I don't really use this though.
-     ("W" "Work Tasks" tags-todo "+work-email")))
+		 ("W" "Work Tasks" tags-todo "+work-email")))
 
 	;; Capturing tasks, journal entries etc is super useful, and this setup allows for that from whereever
-  (org-capture-templates
-   `(("t" "Tasks / Projects")
+	(org-capture-templates
+	 `(("t" "Tasks / Projects")
 		 ;; Capturing a todo-item is super simple. Add some extra information in the entry, such as
 		 ;; where the todo was captured from. Useful for making notes to get back to some piece of
 		 ;; code or whatnot.
-     ("tt" "Task" entry (file+olp org-todo-file "Inbox")
-      "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+		 ("tt" "Task" entry (file+olp org-todo-file "Inbox")
+			"* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
 
 		 ;; Capturing journal entries is something I mostly use for note-taking
-     ("j" "Journal Entries")
-     ("jj" "Journal" entry
-      (file+olp+datetree org-journal-file)
-      "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
-      :clock-in :clock-resume
-      :empty-lines 1)
-     ("jm" "Meeting" entry
-      (file+olp+datetree org-journal-file)
-      "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
-      :clock-in :clock-resume
-      :empty-lines 1)
+		 ("j" "Journal Entries")
+		 ("jj" "Journal" entry
+			(file+olp+datetree org-journal-file)
+			"\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+			:clock-in :clock-resume
+			:empty-lines 1)
+		 ("jm" "Meeting" entry
+			(file+olp+datetree org-journal-file)
+			"* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+			:clock-in :clock-resume
+			:empty-lines 1)
 
 		 ;; Used to use this for email-handling as part of Org, but don't really do that any more.
-     ("w" "Workflows")
-     ("we" "Checking Email" entry (file+olp+datetree org-todo-file)
-      "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)))
+		 ("w" "Workflows")
+		 ;;		 ("we" "Checking Email" entry (file+olp+datetree org-todo-file)
+		 "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1))
 
-  :config
+	:config
 	;; Making org-mode just a little bit pretties
-  (setq org-ellipsis " ▾")
+	(setq org-ellipsis " ▾")
 
 	;; Add timer to logging items. Might be useful some day.
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
+	(setq org-log-done 'time)
+	(setq org-log-into-drawer t)
 
 	;; Set files that are read as part of the agenda views
-  (setq org-agenda-files '(org-todo-file org-archive-file))
+	(setq org-agenda-files (list org-todo-file org-archive-file))
 
 	;; I use these states for all my tasks. So those four states represent a tasks normal progression.
-  (setq org-todo-keywords
-    '((sequence "TODO(t)" "NEXT(n)" "ACTIVE(a)" "|" "DONE(d!)")))
+	(setq org-todo-keywords
+	'((sequence "TODO(t)" "NEXT(n)" "ACTIVE(a)" "|" "DONE(d!)")))
 
 	;; Lets me conveniently choose which file to refile tasks to, eg after thery have been done,
 	;; plus to which section in those files they should be refiled.
-  (setq org-refile-targets
-    '(("todo-archive.org" :maxlevel . 1)
-      ("todo.org" :maxlevel . 1)))
+	(setq org-refile-targets
+	'(("todo-archive.org" :maxlevel . 1)
+		("todo.org" :maxlevel . 1)))
 
-  ;; Save Org buffers after refiling. Considering saving all buffers on _any_ changes, but that can
+	;; Save Org buffers after refiling. Considering saving all buffers on _any_ changes, but that can
 	;; be added later.
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+	(advice-add 'org-refile :after 'org-save-all-org-buffers)
 
 	;; Some tags that can be useful to quick-select
-  (setq org-tag-alist
-    '((:startgroup)
-       ; Put mutually exclusive tags here
-       (:endgroup)
-       ("@errand" . ?E)
-       ("@home" . ?H)
-       ("@work" . ?W)
-       ("agenda" . ?a)
-       ("planning" . ?p)
-       ("publish" . ?P)
-       ("batch" . ?b)
-       ("note" . ?n)
-       ("idea" . ?i)))
+	(setq org-tag-alist
+		'((:startgroup)
+					; Put mutually exclusive tags here
+			(:endgroup)
+			("@errand" . ?E)
+			("@home" . ?H)
+			("@work" . ?W)
+			("agenda" . ?a)
+			("planning" . ?p)
+			("publish" . ?P)
+			("batch" . ?b)
+			("note" . ?n)
+			("idea" . ?i)))
 
 	;; Bing a convenient combo for capturing tasks
-  (define-key global-map (kbd "C-c t")
-    (lambda () (interactive) (org-capture nil "tt"))))
+	(define-key global-map (kbd "C-c t")
+		(lambda () (interactive) (org-capture nil "tt"))))
 
 ;; YAML is used all the time, so make sure that is available
 (use-package yaml-mode)
@@ -170,10 +167,11 @@
 ;; people to set their preferred _visual_ indentation width to whatever makes it easiest
 ;; for them to read the code.
 (setq-default indent-tabs-mode t)
-;; I prefer 2 char visual indentation, but I think most people like 4
 
-;; Make sure cperl-mode is used instead of perl-mode
+;; I prefer 2 char visual indentation, but I think most people like 4
 (setq-default tab-width 2)
+;; Make sure cperl-mode is used instead of perl-mode
+
 (fset 'perl-mode 'cperl-mode)
 (use-package cperl-mode
 	:custom
